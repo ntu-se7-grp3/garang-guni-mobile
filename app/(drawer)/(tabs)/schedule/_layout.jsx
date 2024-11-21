74368db72ed5028901cc5cf29356a09fe41abd93
Lolfoollor <L_Z_F47@hotmail.com>
2024-11-22 00:49:04 +0800

    Remove auto routing to Schedule

--- app/(drawer)/(tabs)/schedule/_layout.jsx
+++ app/(drawer)/(tabs)/schedule/_layout.jsx
@@ -1,16 +1,6 @@
-import { Stack, useFocusEffect, useRouter } from "expo-router";
-import { useCallback } from "react";
-import { routes } from "../../../../constants/routes";
+import { Stack } from "expo-router";
 
 export default function ScheduleLayout() {
-  const router = useRouter();
-
-  useFocusEffect(
-    useCallback(() => {
-      router.replace(routes.BOOKINGS);
-    }, [])
-  );
-
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Schedule" options={{ headerShown: false }} />
